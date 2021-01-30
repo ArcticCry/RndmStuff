defc09775e8c37b826f1eda09f28a5866f99cb6b
ArcticCry <38411865+ArcticCry@users.noreply.github.com>
2021-01-30 02:35:29 +0100

    Update HowManyDivisors.java

--- HowManyDivisors.java
+++ HowManyDivisors.java
@@ -5,50 +5,50 @@ public class HowManyDivisors {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter numbers from which you want divisor, 1-100 only (inclusive). You can enter 3 numbers, if want less, then just type 0 instead");
+        System.out.println("Enter 3 numbers from which you want to know its amount of divisors.\nAny number between 1-100 (inclusive)");
         System.out.print("Number 1: ");
         int a = sc.nextInt();
         while (a <1 || a >100) {
-            System.out.println("1please enter a number between 1-100 (inclusive) you raccoon");
+            System.out.print("Please enter a number between 1-100 (inclusive) you raccoon: ");
             a = sc.nextInt();
         }
         System.out.print("Number 2: ");
         int b = sc.nextInt();
         while (b <1 || b >100) {
-            System.out.println("2please enter a number between 1-100 (inclusive) you raccoon");
+            System.out.print("Please enter a number between 1-100 (inclusive) you raccoon: ");
             b = sc.nextInt();
         }
         System.out.print("Number 3: ");
         int c = sc.nextInt();
         while (c <1 || c >100) {
-            System.out.println("3please enter a number between 1-100 (inclusive) you raccoon");
+            System.out.print("Please enter a number between 1-100 (inclusive) you raccoon: ");
             c = sc.nextInt();
         }
+        //code above gets 3 numbers from the user, and makes sure they're not below 1 or above 100
 
-        int l=1,m=1,n=1;
+        int l=0,m=0,n=0; //these ints will be the amount of divisors
 
-        for (int i=1; i<a; i++) {
+        //these for loops go through all numbers from 1- to the user chosen number,
+        // and increases value of int l,m,n every time it recognizes a valid divisor
+        for (int i=1; i<=a; i++) {
             if (a % i==0) {
                 l++;
             }
         }
-        for (int i=1; i<b; i++) {
+        for (int i=1; i<=b; i++) {
             if (b % i==0) {
                 m++;
             }
         }
-        for (int i=1; i<c; i++) {
+        for (int i=1; i<=c; i++) {
             if (c % i==0) {
                 n++;
             }
         }
 
-
-
-        System.out.println("amount of divisors of " + a + " is: " + l);
-        System.out.println("amount of divisors of " + b + " is: " + m);
-        System.out.println("amount of divisors of " + c + " is: " + n);
-
-
+        //prints out final answer
+        System.out.println(a + " has " + l + " divisors.");
+        System.out.println(b + " has " + m + " divisors.");
+        System.out.println(c + " has " + n + " divisors.");
     }
 }
